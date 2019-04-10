275bb5f419297ffa1cc349a6bdb68e16ee24db14
Ainhoa <ainhoa@Mini-de-Carlos.lan>
2019-04-10 12:33:57 +0200

    VIDAA intelectual property added

--- src/components/pl_component_viewer/pl_component_viewer_intersection/pl_component_viewer_contour_actions_2D.js
+++ src/components/pl_component_viewer/pl_component_viewer_intersection/pl_component_viewer_contour_actions_2D.js
@@ -1,3 +1,33 @@
+/*
+# VIDAA is (c) BCNMedTech, UNIVERSITAT POMPEU FABRA
+#
+# VIDAA is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# VIDAA platform is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details. The software
+# cannot be used for clinical decision making.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Authors:
+# Ainhoa Marina Aguado Martin
+# Carlos Yagüe Méndez
+# Andy Luis Olivares
+# Oscar Camara
+# Xavier Freixa
+# Contributors: 
+# Ibai Genua
+# Álvaro Fernández-Quilez
+# Jordi Mill
+# María del Pilar García
+*/
+
 import { isObjectEmpty, isObjectAnArray } from './../../../modules/pl_module_object';
 
 var THREE = require('three');
